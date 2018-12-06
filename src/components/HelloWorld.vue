610d2ef36f8d041d01c6911b39d064c23a550705
Andre Dargains <andre.dargains@gmail.com>
2018-12-06 17:01:52 +0000

    ADD: initial commit;

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="home">
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
