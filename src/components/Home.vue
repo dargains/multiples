cfeb023d569ad169b542676fd70d22477af02b8a
Andre Dargains <andre.dargains@gmail.com>
2018-12-06 17:33:07 +0000

    UPD: getMultiples method;

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,44 @@
 <template>
-  <section class="home">
-
+  <section>
+    <ul class="grid">
+      <ListItem v-for="number in numberList" :key="number" :number="number"/>
+    </ul>
   </section>
 </template>
 
 <script>
+import ListItem from './ListItem';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    ListItem
+  },
+  mounted() {
+    this.getMultiples(8, 0, 600);
+  },
+  data() {
+    return {
+      numberList: []
+    }
+  },
+  methods: {
+    getMultiples(number, from, to) {
+      const numberList = [];
+      for (let i = from; i <= to; i++) {
+        i%number === 0 && numberList.push(i)
+      }
+      this.numberList = numberList;
+    }
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="css">
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(8, 1fr);
+    grid-row-gap: 10px;
+    grid-column-gap: 20px;
+  }
 </style>
---
+++ src/components/ListItem.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <li class="listItem">{{number}}</li>
+</template>
+
+<script>
+export default {
+  name: 'ListItem',
+  props: ['number']
+}
+</script>
+
+<style lang="css">
+  .listItem {
+    border-radius: 4px;
+    background-color: lightblue;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
