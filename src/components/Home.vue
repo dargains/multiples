892ac5b8aa4b8ea2a2b9abb34dbfac4868e23e8e
Andre Dargains <andre.dargains@gmail.com>
2019-01-03 11:40:40 +0000

    RFC: grid improvements;

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -52,40 +52,10 @@ export default {
 </script>
 
 <style lang="scss">
-@mixin mq($smallerValue,$biggerValue) {
-  @media (min-width: #{$smallerValue}) and (max-width: $biggerValue) {
-    @content;
-  }
-}
-.main {
-  // width: 100%;
-  // margin: 0 auto;
-  // max-width: 1280px;
-  // padding: 40px 10%;
-}
 .grid {
   display: grid;
-  grid-template-columns: repeat(10, 1fr);
+  grid-template-columns: repeat( auto-fit, minmax(150px, 1fr) );
   grid-row-gap: 0;
   grid-column-gap: 0;
-  @include mq(0, 1280px) {
-    grid-template-columns: repeat(8, 1fr);
-  }
-  @include mq(0, 1024px) {
-    grid-template-columns: repeat(6, 1fr);
-  }
-  @include mq(0, 820px) {
-    grid-template-columns: repeat(4, 1fr);
-  }
-  @include mq(0, 658px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-  // @include mq(0, 496px) {
-  //   grid-template-columns: repeat(2, 1fr);
-  // }
-  @include mq(0, 340px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-
 }
 </style>
